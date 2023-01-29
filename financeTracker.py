6f1dbff1719ef38c613a6d787d7ea86e7ece4ded
Alif Hossain Sajib <95392853+AlifHossain27@users.noreply.github.com>
2023-01-29 19:42:34 +0600

    Update financeTracker.py file

--- financeTracker.py
+++ financeTracker.py
@@ -16,30 +16,108 @@ class FinanceTracker():
         return self.cur
 
     def dailyExpenditure(self):
+        # Categories List
+        categoryList = ["Add Category", "Proceed"]
         # Getting user input
         date =  input("Enter the date (YYYY-MM-DD): ")
         earn = int(input("Amount of money earned: "))
         spent = int(input("Amount of money spent: "))
-        saved = int(input("Amount of money saved: "))
+        saved = int(earn-spent)
 
         if date == "":
             date = datetime.date.today()
             # Inserting the Data into the database
             self._conn_database()
             self.cur.execute("INSERT INTO DailyExpense (date, earn, spent, saved) VALUES (?,?,?,?)", (date, earn, spent, saved))
-            self.conn.commit()   
+            self.conn.commit()
+
+            # Expense Category
+            subMenuExit = False
+            while subMenuExit == False:
+                self._conn_database()
+                self.cur.execute("SELECT DISTINCT category FROM ExpenseList")
+                categories= self.cur.fetchall()
+                for category in categories:
+                    categoryList.append(functools.reduce(operator.add, (category)))
+                categoryList = set(categoryList)
+                categoryList = list(categoryList)
+                categoryList.sort(key='Add Category'.__eq__)
+                categoryList.sort(key='Proceed'.__eq__)
+                new_options = categoryList
+                subMenu = TerminalMenu(new_options, title = "Select Category")
+                new_optionsIndex = subMenu.show()
+                new_optionsChoice = new_options[new_optionsIndex]
+                if new_optionsChoice == "Proceed":
+                    subMenuExit = True
+                    break
+                if new_optionsChoice == "Add Category":
+                    os.system("clear")
+                    new_category = input("Enter a new Category: ")
+                    desc = input("Enter a description: ")
+                    amount = int(input("Enter the amount: "))
+                    
+                    self._conn_database()
+                    self.cur.execute("INSERT INTO ExpenseList (date, category, description, amount) VALUES (?, ?, ?, ?)", (date, new_category, desc, amount))
+                    self.conn.commit()
+                else:
+                    os.system("clear")
+                    new_category = new_optionsChoice
+                    desc = input("Enter a description: ")
+                    amount = int(input("Enter the amount: "))
+                    
+                    self._conn_database()
+                    self.cur.execute("INSERT INTO ExpenseList (date, category, description, amount) VALUES (?, ?, ?, ?)", (date, new_category, desc, amount))
+                    self.conn.commit()
 
         else:
             # Inserting the Data into the database
             self._conn_database()
             self.cur.execute("INSERT INTO DailyExpense (date, earn, spent, saved) VALUES (?,?,?,?)", (date, earn, spent, saved))
             self.conn.commit()
+
+            # Expense Category
+            subMenuExit = False
+            while subMenuExit == False:
+                self._conn_database()
+                self.cur.execute("SELECT DISTINCT category FROM ExpenseList")
+                categories= self.cur.fetchall()
+                for category in categories:
+                    categoryList.append(functools.reduce(operator.add, (category)))
+                categoryList = set(categoryList)
+                categoryList = list(categoryList)
+                categoryList.sort(key='Add Category'.__eq__)
+                categoryList.sort(key='Proceed'.__eq__)
+                new_options = categoryList
+                subMenu = TerminalMenu(new_options, title = "Select Category")
+                new_optionsIndex = subMenu.show()
+                new_optionsChoice = new_options[new_optionsIndex]
+                if new_optionsChoice == "Proceed":
+                    subMenuExit = True
+                    break
+                if new_optionsChoice == "Add Category":
+                    os.system("clear")
+                    new_category = input("Enter a new Category: ")
+                    desc = input("Enter a description: ")
+                    amount = int(input("Enter the amount:"))
+                    
+                    self._conn_database()
+                    self.cur.execute("INSERT INTO ExpenseList (date, category, description, amount) VALUES (?, ?, ?, ?)", (date, new_category, desc, amount))
+                    self.conn.commit()
+                else:
+                    os.system("clear")
+                    new_category = new_optionsChoice
+                    desc = input("Enter a description: ")
+                    amount = int(input("Enter the amount: "))
+                    self._conn_database()
+                    self.cur.execute("INSERT INTO ExpenseList (date, category, description, amount) VALUES (?, ?, ?, ?)", (date, new_category, desc, amount))
+                    self.conn.commit()
             
 
     def _stats(self):
         month = input("Enter the month (MM): ")
         year = input("Enter the year (YYYY): ")
-        print("Finance Stats")
+        os.system("clear")
+        print("=====Finance Stats=====")
 
         if month == "" or year == "":
             month = datetime.datetime.now().strftime('%m')
@@ -55,7 +133,7 @@ class FinanceTracker():
             self.cur.execute("SELECT category, sum(amount) FROM ExpenseList WHERE strftime('%m', date) = ? AND strftime('%Y', date) = ? GROUP BY category", (month, year))
             expenseData = self.cur.fetchall()
             for expense in expenseData:
-                print(f"Categoty: {expense[0]}, Total: {expense[1]}")
+                print(f"Categoty: {expense[0]} -> Total: {expense[1]}")
             
         else:
             self._conn_database()
@@ -72,7 +150,7 @@ class FinanceTracker():
                 print(f"Categoty: {expense[0]}, Total: {expense[1]}")
     
     def _get_menu(self):
-        options = ["Today's Expenditure", "Expenditure", "Stats","Quit"]
+        options = ["Expenditure", "Stats","Quit"]
         quitting = False
 
         mainMenu = TerminalMenu(options, title = "=====Finance Tracker=====")
@@ -84,69 +162,14 @@ class FinanceTracker():
                 self._conn_database()
                 self.conn.close()
                 quitting = True
-            if optionsChoice == "Today's Expenditure":
-                os.system("clear")
-                self.dailyExpenditure()
-
             if optionsChoice == "Expenditure":
-                date = input("Enter the date (YYYY-MM-DD): ")
-                categotyList = []
-                if date == "":
-                    date = datetime.date.today()
-                    self._conn_database()
-                    self.cur.execute("SELECT DISTINCT category FROM ExpenseList")
-                    categories= self.cur.fetchall()
-                    for category in categories:
-                        categotyList.append(functools.reduce(operator.add, (category)))
-                    new_options = categotyList
-                    new_options.append("Add Category")
-                    subMenu = TerminalMenu(new_options, title = "Select Category")
-                    new_optionsIndex = subMenu.show()
-                    new_optionsChoice = new_options[new_optionsIndex]
-                    if new_optionsChoice == "Add Category":
-                        new_category = input("Enter a new Category: ")
-                        desc = input("Enter a description: ")
-                        amount = int(input("Enter the amount: "))
-                        
-                        self._conn_database()
-                        self.cur.execute("INSERT INTO ExpenseList (date, category, description, amount) VALUES (?, ?, ?, ?)", (date, new_category, desc, amount))
-                        self.conn.commit()
-                    else:
-                        new_category = new_optionsChoice
-                        desc = input("Enter a description: ")
-                        amount = int(input("Enter the amount:"))
-                        
-                        self._conn_database()
-                        self.cur.execute("INSERT INTO ExpenseList (date, category, description, amount) VALUES (?, ?, ?, ?)", (date, new_category, desc, amount))
-                        self.conn.commit()
-                else:
-                    self._conn_database()
-                    self.cur.execute("SELECT DISTINCT category FROM ExpenseList")
-                    categories= self.cur.fetchall()
-                    for category in categories:
-                        categotyList.append(functools.reduce(operator.add, (category)))
-                    new_options = categotyList
-                    new_options.append("Add Category")
-                    subMenu = TerminalMenu(new_options, title = "Select Category")
-                    new_optionsIndex = subMenu.show()
-                    new_optionsChoice = new_options[new_optionsIndex]
-                    if new_optionsChoice == "Add Category":
-                        new_category = input("Enter a new Category: ")
-                        desc = input("Enter a description: ")
-                        amount = int(input("Enter the amount:"))
-                        
-                        self._conn_database()
-                        self.cur.execute("INSERT INTO ExpenseList (date, category, description, amount) VALUES (?, ?, ?, ?)", (date, new_category, desc, amount))
-                        self.conn.commit()
-                    else:
-                        new_category = new_optionsChoice
-                        desc = input("Enter a description: ")
-                        amount = int(input("Enter the amount:"))
-                        
-                        self._conn_database()
-                        self.cur.execute("INSERT INTO ExpenseList (date, category, description, amount) VALUES (?, ?, ?, ?)", (date, new_category, desc, amount))
-                        self.conn.commit()
-
+                os.system("clear")
+                try:
+                    self.dailyExpenditure()
+                except Exception as e:
+                    print(e)
+                    #print("An Error occurred!!! USE THE CORRECT DATATYPE")
+                    continue
             if optionsChoice == "Stats":
                 os.system("clear")
                 self._stats()
